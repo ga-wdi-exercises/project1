a3b9b292b5571f692c0ed975c13a29d86f20bb9c
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-23 16:42:58 -0500

    tested whichClickAreWeOne var

--- script.js
+++ script.js
@@ -10,22 +10,26 @@ for (var i=0;i<select.length;i++){
   select[i].addEventListener("click",function(){
     console.log(this.getAttribute("name"));
 
-if(whichClickAreWeOn == 1){
-  whichClickAreWeOn == 2
+if(whichClickAreWeOn === 1){
+  console.log("clickOne");
+  whichClickAreWeOn ++;
+  }
+if(whichClickAreWeOn === 2){
+  console.log("clickTwo");
+  //compare click1 and click2
+  //if click1===click2{Success!}
+  //else{code for failure}
+
+
+
+
+
+
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-whichClickAreWeOn =1;
+whichClickAreWeOn ===1;
 
   });
 }

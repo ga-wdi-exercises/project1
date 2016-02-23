0be79c609781b6f2ac8f705c22c1e236f3f49342
YOUR FULL NAME <farishta.haider@gmail.com>
2016-02-23 17:29:37 -0500

    Updates

--- readme.md
+++ readme.md
@@ -184,3 +184,13 @@ I am going to start small with just 3 topics with one question. If time permits,
 bronze plan : trivia questions on the page
 silver plan: being able to interact and answer the questions
 gold plan: for the jquery animation to work and flow easily.
+Platnium - timer , scoreboard and more questions.
+
+
+
+2/23/16
+Achieved bronze level. Now need to make it interactive.
+
+Changing from jQuery to JavaScript DOM in the script.
+
+Need to list out variables. Listing them in the psuedocode.
--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var count=30;
+var instructions = document.getElementsByClassName(".instructions")
+var question = document.getElementsByClassName(".question")
+var optionA = button.getElementById(".optionA")
+var optionB = button.getElementById(".optionB")
+var optionC = button.getElementById(".optionC")
+var optionD = button.getElementById(".optionD")
+var Correctanswer = document.querySelector(".answer")
+var platButton = document.getElementById(".playbutton")
 
-var counter=setInterval(timer, 1000);
 
-function timer()
-{
-  count=count-1;
-  if (count <= 0)
-  {
-     clearInterval(counter);
-     return;
-  }
+optionA.addEventListener("onclick"displayDate())
+optionB.addEventListener("click", changeWhite )
+optionC.addEventListener("click", changeBlue )
+optionD.addEventListener("click", changeYellow )
 
- document.getElementById("timer").innerHTML=count + " secs"; 
+function hide(){
+  button.body.style.backgroundColor = "red"
+  button.body.style.color = "white"
 }
-    $('.instructions').click(function() {
-      $('.playButton').fadeOut('fast', 1.00);
-    });
-});

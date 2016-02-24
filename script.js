3212350e41897805dfb2dadb467de3a2a5a975a7
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-24 10:04:56 -0500

    added second sidebar

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-firstClick =
-secondClick =
-whichClickAreWeOn = 1
 
+whichClickAreWeOn = 1
+firstGuess=null
+secondGuess=null
+function compare(){console.log("compare successful")}
 
 var select = document.querySelectorAll(".clickable");
 console.log(select);
 
 for (var i=0;i<select.length;i++){
   select[i].addEventListener("click",function(){
-    console.log(this.getAttribute("name"));
+    //console.log(this.getAttribute("name"));
+      if (firstGuess == null){
+    firstGuess=this.getAttribute("name");
+    console.log("Your first guess is "+ firstGuess);}
+      else{
+        secondGuess=this.getAttribute("name");
+        console.log("your second guess is "+secondGuess);
+
+        compare();
+        
+  firstGuess = null;
+  secondGuess = null;
+    }
+      })
+}
+
+    /*
 
 if(whichClickAreWeOn === 1){
-  console.log("clickOne");
   whichClickAreWeOn ++;
   }
 if(whichClickAreWeOn === 2){
-  console.log("clickTwo");
+  //secondGuess=
+  //console.log("You guessed " + secondGuess);
   //compare click1 and click2
   //if click1===click2{Success!}
   //else{code for failure}
 
-whichClickAreWeOn ===1;
-
-
-
-
-
-
+whichClickAreWeOn =1;
 
 }
   });
-}
+} */

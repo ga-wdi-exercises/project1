ea078890963c30ca20e74520f2641f7adfc393c6
YOUR FULL NAME <farishta.haider@gmail.com>
2016-02-24 14:55:56 -0500

    next commit

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ i = 0;
 //var p1 = document.getElementById(".p1")
 //var b1 = document.getElementById(".b1")
 
-question = ["Which actor has a game based off connecting other celebrities to him in 6 degrees?", "Leonardo DiCaprio won an Oscar for which role?", "Which movie was nominated for the most Oscars?", "Which of these actors has not received an EGOT?", "Which movies is not based off a book?"];
-optionA = ["Kevin Bacon", "None of the above", "Titanic", "Kate Winslet", "Matrix"];
-optionB = ["Tom Cruise", "Django Unchained", "The Godfather", "Audrey Hepburn","Mrs. Doubtfire"];
-optionC = ["Samuel L. Jackson", "Titanic", "Citizen Kane", "Whoopi Goldberg", "Pitch Perfect"];
-optionD = ["Morgan Freeman", "The Wolf of Wall Street", "Star Wars", "Tracy Jordan", "Die Hard"];
-correctAnswer = ["Kevin Bacon", "None of the above", "Titanic", "Kate Winslet", "Matrix"];
+question = ["Which actor has a game based off connecting other celebrities to him in 6 degrees?", "Leonardo DiCaprio won an Oscar for which role?", "Which movie was nominated for the most Oscars?", "Which of these actors has not received an EGOT?", "Which movies is not based off a book?", "Who has hosted Saturday Night Live the most times?", "Who has been banned from SNL?", "Who has not been an anchor on Weekend Update?", "Who was not part of the original SNL cast?", "Who was been a SNL cast member the longest?",
+"Who is singer Elle King's famous father?", "Which of the following fellas did not date Taylor Swift?", "Who is Gwen Stefani's current beau?", "What diet plan is Oprah currently following?", "Who broke up with their ex by ghosting them?"];
+optionA = ["Kevin Bacon", "None of the above", "Titanic", "Kate Winslet", "Matrix", "Alec Baldwin", "Adrien Brody", "Adam Sandler", "Steve Martin", "Darrell Hammond", "Rob Schneider", "Dave Franco", "Blake Shelton", "Weight Watchers", "Charlize Theorn"];
+optionB = ["Tom Cruise", "Django Unchained", "The Godfather", "Audrey Hepburn","Mrs. Doubtfire", "Steve Martin", "Kanye West", "Jimmy Falon", "John Belushi", "Kenan Thompson", "Adam Sandler", "Harry Styles", "Gavin Rossdale", "Nutri-System", "Taylor Swift"];
+optionC = ["Samuel L. Jackson", "Titanic", "Citizen Kane", "Whoopi Goldberg", "Pitch Perfect", "John Goodman", "Miley Cryus", "Dan Aykroyd", "Chevy Chase", "Seth Meyers", "Joey King", "John Mayer", "Adam Levine", "The Atkins Diet Program", "John Mayer"];
+optionD = ["Morgan Freeman", "The Wolf of Wall Street", "Star Wars", "Tracy Jordan", "Die Hard"], ["Christopher Walken", "Paris Hilton", "Chevy Chase", "Dan Aykroyd", "Fred Armisen"], ["Elvis Presley", "Jake Gyllenhaal", "Chris Martin", "Jenny Craig", "Katy Perry"];
+correctAnswer = ["Kevin Bacon", "None of the above", "Titanic", "Kate Winslet", "Matrix", "Alec Baldwin", "Adrien Brody", "Adam Sandler", "Steve Martin", "Darrell Hammond", "Rob Schneider", "Dave Franco", "Blake Shelton", "Weight Watchers", "Charlize Theorn"];
 
 function questionAnswers(){
   question1.innerHTML = question[i];
@@ -29,20 +30,22 @@ function questionAnswers(){
   optionD1.innerHTML = optionD[i];
 }
 
-// function next (){
-// for (i = 0; i < question.length; i++) {
-//   if (question[i] == 5) {
-//     break;
-//   }
-// }
-// }
 
-// question1.addEventListener("click", next);
-// optionA1.addEventListener("click", next);
-// optionB1.addEventListener("click", next);
-// optionC1.addEventListener("click", next);
-// optionD1.addEventListener("click", next);
-// correctAnswer1.addEventListener("click", next);
+question1.addEventListener("click", next);
+optionA1.addEventListener("click", next);
+optionB1.addEventListener("click", next);
+optionC1.addEventListener("click", next);
+optionD1.addEventListener("click", next);
+
+function next (){
+for (i = 0; i < question1.length; i++) {
+  if (question1[i] === 15) {
+    break;
+  }
+}
+}
+
+//correctAnswer1.addEventListener("click", next);
 
 // show correct answer in green & incorrect in red//
 // }
@@ -55,19 +58,5 @@ questionAnswers()
 next()
 }
 trivia()
-//round 2 start//
-//for (i = 0; i < a.length; i++) {
-//  if (a[i] == theValue) {
-//    break;
-//  }
-//}
-//end round 2//
-//start round 3//
-//for (i = 0; i < a.length; i++) {
-//  if (a[i] == theValue) {
-//    break;
-//  }
-//}
 //end of round 3 & game
-
 //prompt outcome//

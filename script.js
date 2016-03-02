75e3573b648c33b361c03c764537919442109e51
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-02 15:11:04 -0500

    called game function

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 $(document).ready(function(){
 
-firstGuess=null
-secondGuess=null
-
-function compare(){
-  if(firstGuess === secondGuess){
-    alert("You got it! Good job!");
-  }else {alert("Nope! Try again! ");}
-  }
-//++++++++++++
+function firstNameBasis(){
+  var firstGuess=null;
+  var secondGuess=null;
+    function compare(){
+      if(firstGuess === secondGuess){
+        alert("You got it! Good job!");
+      }else {
+        alert("Nope! Try again! ");
+      }
+    }
 var select = document.querySelectorAll(".clickable");
-console.log(select);
 
 for (var i=0; i<select.length; i++){
   select[i].addEventListener("click",function(){
-    //console.log(this.getAttribute("name"));
       if (firstGuess == null){
     firstGuess=this.getAttribute("name");
     console.log("Your first guess is "+ firstGuess);}
       else{
         secondGuess=this.getAttribute("name");
         console.log("your second guess is "+secondGuess);
-//++++++call compare function (global) ++++++ //
+
             compare();
-//++++now, get how cool this is! Reset your variables so you can do it over and over!
+
   firstGuess = null;
   secondGuess = null;
-  } // caps the else function.
-}) // caps the eventListener function
-}//caps the hole for loop that sets the "this" and the for/eventListener function.
-
+  }
+})
+}
+}firstNameBasis();
 });

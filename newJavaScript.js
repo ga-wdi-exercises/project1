ca99f227f799128dc3aed69938b4293fb0f88282
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-02 14:56:57 -0500

    create newJavaScript.js file

--- newJavaScript.js
+++ newJavaScript.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
 
+
+// var wdiNames = ["Adrian", "Alexa", "Andrew", "Anissa","Becky","Brendan","Caitlin","Charles","Christine","Clarissa","Craig","Farishta","Isaac","Jeffrey","Joe","John","Keith","Lyndsey","Matt","Melissa","Mohamed","Patrick","Robel"
+// ,"Robin","Sanjaya","Toni","Vijay","Winisha","Adam","Bipin","Brian","Brittany","Casey","Fatai","Hilary","Jesse","Kabir","Kaira","Kimberly","Matthew","Maureen","Nick","Nyron","Ryan"];
+//
+// wdiNames.forEach(function(name){
+//   var card = document.createElement("div");
+//   card.innerHTML=name;
+//   card.class
+//   document.body.appendChild(card);
+//   console.log(name);
+// })
+// };
+// var randomName = wdiNames[Math.floor(Math.random() * wdiNames.length)];
+//
+// var NameTags = document.querySelectorAll(".NameTags");
+// i=0;
+// function generateNames(i){
+//   NameTags.innerHTML=randomName[i];
+//   generateNames();
+// }
+
+
+
 firstGuess=null
 secondGuess=null
 

2627b4428161e9685db2be30c787353dcea6a1da
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-30 17:53:15 -0400

    refactor

---
+++ refractor.js
@@ -0,0 +1,10 @@
+$(document).ready(function()){
+  var Game (round[]){
+  this.round: [0,1,2]
+  this.title: [],
+  this.question: [],
+  this.text: ,
+  this.answerOptions:"",
+  this.correct: "",
+}
+}

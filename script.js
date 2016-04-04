944d9e7fba7d45a30ef4f647d8e89e72bb244e2e
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-03 21:39:53 -0400

    game working again

--- script.js
+++ script.js
@@ -15,19 +15,6 @@ function trivia(){
   i = 0;
   score = 0
 
-  // var question1 = document.querySelector(".question");
-  // var optionA1 = document.getElementById("optionA");
-  // var optionB1 = document.getElementById("optionB");
-  // var optionC1 = document.getElementById("optionC");
-  // var optionD1 = document.getElementById("optionD");
-  // var correctAnswer1 = document.querySelector(".correctAnswer");
-  // //var incorrectAnswer1 = document.querySelector(".incorrectAnswer");
-  // var options = document.querySelectorAll(".answerOptions");
-  // console.log(options);
-  // //var arrayLength = question.length;
-  // //var playerselection = []
-
-
   question = ["ROUND 1 </br> TV & Movies", "Which actor has a game based </br> off connecting any celebrity</br> to him in 6 degrees?", "For which role did </br> Leonardo DiCaprio </br> win an Oscar?", "Which movie has </br> won the </br> most Oscars?", "Which of these </br> actors has not </br> received an EGOT?", "Which movie </br> is not </br> based off a book?", "ROUND 2 </br> Saturday Night </br> Live", "Who has hosted </br> Saturday Night Live </br> the most times?", "Who has been </br> banned from <br> SNL?", "Who has not </br> been an anchor on </br> Weekend Update?", "Who was not </br> part of the <br> original SNL cast?", "Who was been a SNL </br> cast member the longest?", "ROUND 3 </br> Celebrity </br> Gossip",
   "Who is singer Elle King's </br> famous father?", "Which of the following </br> fellas did not date </br> Taylor Swift?", "Who is Gwen Stefani's </br> current beau?", "What diet plan is Oprah </br> currently following?", "Who broke up with their </br> ex by ghosting them?", "GAME OVER"];
   optionA = ["Press to Play","Kevin Bacon", "None of these options", "Titanic", "Kate Winslet", "Matrix", "Press to Play", "Alec Baldwin", "Adrien Brody", "Adam Sandler", "Steve Martin", "Darrell Hammond", "Press to Play", "Rob Schneider", "Dave Franco", "Blake Shelton", "Weight Watchers", "Charlize Theorn"];
@@ -44,15 +31,6 @@ function trivia(){
     optionC1.innerHTML = optionC[i];
     optionD1.innerHTML = optionD[i];
   }
-
-  // function questionAnswers(i){
-  //   question1.innerHTML = question[i];
-  //   question1.setAttribute("data-index", i);
-  //   optionA1.innerHTML = optionA[i];
-  //   optionB1.innerHTML = optionB[i];
-  //   optionC1.innerHTML = optionC[i];
-  //   optionD1.innerHTML = optionD[i];
-  // }
 
   optionA1.addEventListener("click", function () {
     if (this.classList.contains("correctAnswer")) {
@@ -99,6 +77,7 @@ function trivia(){
     } else {
       questionAnswers(1 + index);
     }
+  }
   questionAnswers(i)
 }
 trivia()

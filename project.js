954f5cbbc477d274d934c38525afe3c0e15c0f25
dfwg4@hotmail.com <dfwg4@hotmail.com>
2016-11-30 08:42:30 -0500

    added image file/var and function placeholders,html&css looking a bit better-put that down

--- project.js
+++ project.js
@@ -13,11 +13,11 @@
 var url = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5/?t=";
 
 $.ajax({
-    type: "GET",
     url: url,
+    type: "GET",
     dataType: "json",
 }).done(function(response){
-        $('h1').html(data.Word);/*('something happened');*/
+        $('h1').html(response.data.Word);/*('something happened');*/
     });
 
 function RandomWordComplete(data) {
@@ -26,8 +26,50 @@ function RandomWordComplete(data) {
 /*
 //test - did not work**************
 var choosenWord = (data.Word);
-
+//****Need to look at above and assign the data.word which was the path
+//to follow with above api to work with the below variables - not so wordy
 console.log(choosenWord);*/
+
+//need the variables will use in functions
+//variable to assign random word to
+
+var questionBank/*data.Word*/ = new [];
+//variable to place individual letters into - an array - will need to split(Python methos) and place letters here
+var wordArray = new [];
+//an array for choosenletters which will use to black out letter
+var previousWordChoices = new [];
+//for number of chances have till hung
+/*var livesLeft = 6;
+//for remaining letters to pick
+var lettersRemaining = Word.Length;
+//a test word to start
+var word = 'tryout';
+//?Displayed Word
+var wordDisplay = '';*/
+var currentWord;
+
+var currentClue;
+
+var wrongAnswerCount;
+
+
+//start creatinf functions
+
+function useWord(){
+
+};
+
+function(){
+
+};
+
+function(){
+
+};
+
+function(){
+
+};
 
 //give user a choose of letters - done w/css/html - need onClick to assign letter as variable
 //to compare to the variables created after random word is generated
